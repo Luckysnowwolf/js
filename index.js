c5d03548c027fd097df02ce86984109c76d468b5
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-10-16 09:00:02 +0200

    added index.html and index.jp

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Helloworld");
+
+
+let firstname = "my name is lucas"
+console.log(firstname);
+
+let age = "my age is 26"
+console.log(age);
+
+let favcolor = "my fav color is red"
+console.log(favcolor);
+
+let number1="the first number 100";
+let number2="the second number 8";
+let number3="the third number 32";
+let addition ="100 + 8 + 32= 140";
+let subtraction ="100 - 8 - 32 = 60";
+let multiplication ="100 x 8 x 32 = 25600";
+let division ="100 / 8 / 32 = 0.390625";
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
