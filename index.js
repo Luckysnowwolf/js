a4901c245e97967126976383c3f13bfd59a1729e
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-10-18 10:14:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 console.log("Helloworld");
 
 
-let firstname = "my name is lucas"
-console.log(firstname);
+let myfirstName = ("Lucas")
+let myAge = (26)
+let myfavoriteColor = ("red")
+console.log(`My name is ${myfirstName}, I am ${myAge} years old. My favorite color is ${myfavoriteColor}.`);
+//console.comment("the console looks up the let conected with the samme name to show that coded number or text to the log that then shows it on your command line")
 
-let age = "my age is 26"
-console.log(age);
-
-let favcolor = "my fav color is red"
-console.log(favcolor);
-
-let num1="100";
-let num2="8";
-let num3="32";
-let addition = num1 + num2 + num3;
-let subtraction = num1 - num2 - num3 ;
-let multiplication = num1 * num2 * num3;
-let division = num1 / num2 / num3 ;
-
-console.log(num1 = 100)
-console.log(num2 = 8 )
-console.log(num3 = 32)
-console.log(addition + add)
-console.log(subtraction + add)
-console.log(multiplication + add)
-console.log(division + add)
+let Num1= (100);
+let Num2= (8);
+let Num3= (32);
+let Addition = (Num1 + Num2 + Num3);
+let Subtraction = (Num1 - Num2 - Num3) ;
+let Multiplication = (Num1 * Num2 * Num3);
+let Division = (Num1 / Num2 / Num3) ;
+//console.comment("its same here as the fists just here the let calcualtes the numbers befor sending it to the log for it to show on the command line")
+console.log(Num1)
+console.log(Num2)
+console.log(Num3)
+console.log(Addition)
+console.log(Subtraction)
+console.log(Multiplication)
+console.log(Division)
